83df3312c6a1f514741c2f98a0ce19d58f70c61f
Andrey <univer1857@gmail.com>
2021-02-16 11:12:15 +0200

    remove commented strings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse (n) {
-  // let splitString = String(n).split('');
-  // let reverseArray = splitString.reverse();
-  // let joinArray = reverseArray.join('');
   return Number(String(Math.abs(n)).split('').reverse().join('')); 
 }
