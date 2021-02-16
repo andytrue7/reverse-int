da5799310c53a10f32cea2a5e156dfeb849e2355
Andrey <univer1857@gmail.com>
2021-02-16 11:10:17 +0200

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  // let splitString = String(n).split('');
+  // let reverseArray = splitString.reverse();
+  // let joinArray = reverseArray.join('');
+  return Number(String(Math.abs(n)).split('').reverse().join('')); 
 }
